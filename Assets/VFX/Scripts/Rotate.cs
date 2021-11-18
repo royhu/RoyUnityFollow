49ddb01e40a1a21d197d0fbfc89a38640c41918e
royhu <royhugangsheng@gmail.com>
2021-11-18 16:03:52 +0800

    VFX sample mesh

--- Assets/VFX/Scripts/Rotate.cs
+++ Assets/VFX/Scripts/Rotate.cs
@@ -7,10 +7,14 @@
 using UnityEngine;
 using System.Collections;
 
-public class Rotate : MonoBehaviour {
-	void Update () {
-		transform.Rotate(Vector3.down * Time.deltaTime * 10);
-		transform.Rotate(Vector3.right * Time.deltaTime * 1);
-		transform.Rotate(Vector3.forward * Time.deltaTime * 8);
+public class Rotate : MonoBehaviour
+{
+	public Vector3 speed = Vector3.one;
+
+	void Update ()
+	{
+		transform.Rotate(Vector3.down * Time.deltaTime * speed.x);
+		transform.Rotate(Vector3.right * Time.deltaTime * speed.y);
+		transform.Rotate(Vector3.forward * Time.deltaTime * speed.z);
 	}
 }
