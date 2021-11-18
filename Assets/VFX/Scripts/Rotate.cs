cdc32230f4423debafeda5d2d356a879f83198dd
royhu <royhugangsheng@gmail.com>
2021-11-18 15:07:05 +0800

    VFX sample mesh, add modle rotating

---
+++ Assets/VFX/Scripts/Rotate.cs
@@ -0,0 +1,16 @@
+﻿/****************************************************************************
+ *
+ * Copyright (c) 2011 CRI Middleware Co., Ltd.
+ *
+ ****************************************************************************/
+
+using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+	void Update () {
+		transform.Rotate(Vector3.down * Time.deltaTime * 10);
+		transform.Rotate(Vector3.right * Time.deltaTime * 1);
+		transform.Rotate(Vector3.forward * Time.deltaTime * 8);
+	}
+}
